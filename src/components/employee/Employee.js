4d417a82622310d59c7e2e3d76f9ddec150251d6
buckleyderekp <buckleyderekp@gmail.com>
2020-04-20 15:32:13 -0500

    initial commit

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="employee">
+        <h3 className="employee__name">Hannah Hall</h3>
+        <div className="employe___location">North Nashville</div>
+    </section>
+)
